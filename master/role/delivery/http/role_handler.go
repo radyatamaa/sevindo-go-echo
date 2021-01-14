081ae62ec5a2031405ecc73b89ac2ae51f4239bb
mfikrihatim <mfikrihatim@gmail.com>
2021-01-14 21:46:49 +0700

    Push CRUD Role

---
+++ master/role/delivery/http/role_handler.go
@@ -0,0 +1,197 @@
+package http
+
+import (
+	"context"
+	"github.com/labstack/echo"
+	"github.com/master/role"
+	"github.com/models"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/go-playground/validator.v9"
+	"net/http"
+	"strconv"
+)
+
+// ResponseError represent the response error struct
+type ResponseError struct {
+	Message string `json:"message"`
+}
+
+// roleHandler  represent the http handler for role
+type roleHandler struct {
+	roleUsecase role.Usecase
+}
+
+// NewroleHandler will initialize the roles/ resources endpoint
+func NewroleHandler(e *echo.Echo, us role.Usecase) {
+	handler := &roleHandler{
+		roleUsecase: us,
+	}
+	e.POST("/master/role", handler.Create)
+	e.PUT("/master/role/:id", handler.UpdateRole)
+	e.DELETE("/master/role/:id", handler.Delete)
+	// e.GET("/countrys/:id/credit", handler.GetCreditByID)
+	e.GET("/master/role/:id", handler.GetDetailID)
+	e.GET("/master/role", handler.List)
+}
+
+func isRequestValid(m *models.NewCommandRole) (bool, error) {
+	validate := validator.New()
+	err := validate.Struct(m)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+func (a *roleHandler) Delete(c echo.Context) error {
+	c.Request().Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	token := c.Request().Header.Get("Authorization")
+
+	if token == "" {
+		return c.JSON(http.StatusUnauthorized, models.ErrUnAuthorize)
+	}
+
+	id := c.Param("id")
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	ids ,_:= strconv.Atoi(id)
+	result, err := a.roleUsecase.Delete(ctx, ids, token)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, result)
+}
+
+func (a *roleHandler) List(c echo.Context) error {
+	c.Request().Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	token := c.Request().Header.Get("Authorization")
+
+	if token == "" {
+		return c.JSON(http.StatusUnauthorized, models.ErrUnAuthorize)
+	}
+
+	qpage := c.QueryParam("page")
+	qperPage := c.QueryParam("size")
+	search := c.QueryParam("search")
+
+	var limit = 20
+	var page = 1
+	var offset = 0
+
+	page, _ = strconv.Atoi(qpage)
+	limit, _ = strconv.Atoi(qperPage)
+	offset = (page - 1) * limit
+
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	result, err := a.roleUsecase.List(ctx, page, limit, offset,search)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, result)
+}
+
+func (a *roleHandler) UpdateRole(c echo.Context) error {
+	c.Request().Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	token := c.Request().Header.Get("Authorization")
+
+	if token == "" {
+		return c.JSON(http.StatusUnauthorized, models.ErrUnAuthorize)
+	}
+
+	id := c.Param("id")
+	var role models.NewCommandRole
+	ids ,_:= strconv.Atoi(id)
+	role.Id = ids
+	err := c.Bind(&role)
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	err = a.roleUsecase.Update(ctx, &role, token)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, role)
+}
+
+func (a *roleHandler) Create(c echo.Context) error {
+	c.Request().Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	token := c.Request().Header.Get("Authorization")
+
+	if token == "" {
+		return c.JSON(http.StatusUnauthorized, models.ErrUnAuthorize)
+	}
+
+
+	var role models.NewCommandRole
+	err := c.Bind(&role)
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	res,err := a.roleUsecase.Create(ctx, &role, token)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, res)
+}
+
+func (a *roleHandler) GetDetailID(c echo.Context) error {
+	c.Request().Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	token := c.Request().Header.Get("Authorization")
+
+	id := c.Param("id")
+
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	ids ,_:= strconv.Atoi(id)
+	result, err := a.roleUsecase.GetById(ctx, ids, token)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, result)
+}
+
+func getStatusCode(err error) int {
+	if err == nil {
+		return http.StatusOK
+	}
+	logrus.Error(err)
+	switch err {
+	case models.ErrInternalServerError:
+		return http.StatusInternalServerError
+	case models.ErrNotFound:
+		return http.StatusNotFound
+	case models.ErrUnAuthorize:
+		return http.StatusUnauthorized
+	case models.ErrConflict:
+		return http.StatusBadRequest
+	case models.ErrBadParamInput:
+		return http.StatusBadRequest
+	default:
+		return http.StatusInternalServerError
+	}
+}
