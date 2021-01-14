68e20ce262ff591b99b6f7a5e1fc4e4965fee5d0
bagusshndr <bagussuhendri19@gmail.com>
2021-01-15 00:37:44 +0700

    PUSH

--- master/article_blog/repository.go
+++ master/article_blog/repository.go
@@ -1,4 +1,4 @@
-package article_category
+package article_blog
 
 import (
 	"github.com/models"
@@ -6,10 +6,10 @@ import (
 )
 
 type Repository interface {
-	GetByID(ctx context.Context, id string) (*models.ArticleCategory, error)
-	Update(ctx context.Context, ar *models.ArticleCategory) error
-	Insert(ctx context.Context, a *models.ArticleCategory) error
-	Delete(ctx context.Context, id string, deleted_by string) error
+	GetByID(ctx context.Context, id int) (*models.ArticleBlog, error)
+	Update(ctx context.Context, ar *models.ArticleBlog) error
+	Insert(ctx context.Context, a *models.ArticleBlog) error
+	Delete(ctx context.Context, id int, deleted_by string) error
 	Count(ctx context.Context) (int, error)
-	List(ctx context.Context, limit, offset int) ([]*models.ArticleCategory, error)
+	List(ctx context.Context, limit, offset int) ([]*models.ArticleBlog, error)
 }
